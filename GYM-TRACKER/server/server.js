761d604dd2723ff3fd2868ac2588f86be0783fcd
Saifi <saififaiziraza@gmail.com>
2026-08-31 23:51:45 +0530

    Fix server path and add dotenv configuration

--- GYM-TRACKER/server/server.js
+++ GYM-TRACKER/server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
-import db from "./db.js";
+import dotenv from "dotenv";
+import db from "../db.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use((req, res, next) => {
   res.setHeader(
